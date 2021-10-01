a41e8c5259b486aa7bc2711b4e2ad1f55f1043e9
Troy Grossi <ttroygrossit@gmail.com>
2021-09-30 18:03:26 -0700

    finished graphql querys and mutations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -14,6 +15,7 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
+    context: authMiddleware
   });
 
   // Start the Apollo server
